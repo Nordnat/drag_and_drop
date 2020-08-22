0d2e1d299a29b48bcc9788b8b167c56729a7afe6
Rafal Kamieniarz <rkamieniarz@gmail.com>
2020-08-22 21:50:34 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+class ProjectInput {
+    templetElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templetElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templetElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput  = new ProjectInput();
